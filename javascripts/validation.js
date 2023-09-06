96470bd0b23ad9c5fc0b29b721a37bde90bd0400
remi <remi.nkjm@gmail.com>
2023-09-06 22:56:11 +0900

    バインディングを使ったさまざまな手法

--- javascripts/validation.js
+++ javascripts/validation.js
@@ -2,5 +2,16 @@ const app = new Vue({
   el: '#app',
   data: {
     errorMessage: 'Eメールが一致しません',
+    formData: {
+      name: '',
+      email: '',
+      emailConfirm: '',
+      content: '',
+    },
+    regions: [
+      { id: 1, name: '東北' } ,
+      { id: 2, name: '関東' } ,
+      { id: 3, name: '関西' } 
+    ]
   }
 })
