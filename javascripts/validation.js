ff385cd52ee41ac9a57db325b9ac171b7d671ba9
remi <remi.nkjm@gmail.com>
2023-09-08 19:18:40 +0900

    Vue.jsプログラムの改修とリファクタリング

--- javascripts/validation.js
+++ javascripts/validation.js
@@ -7,11 +7,33 @@ const app = new Vue({
       email: '',
       emailConfirm: '',
       content: '',
+      region: '',
     },
     regions: [
-      { id: 1, name: '東北' } ,
-      { id: 2, name: '関東' } ,
-      { id: 3, name: '関西' } 
-    ]
-  }
+      { id: 1, name: '東北' },
+      { id: 2, name: '関東' },
+      { id: 3, name: '関西' }
+    ],
+    confirmView: false,
+  },
+  methods: {
+    openCheckArea() {
+      // 要件2の改修
+      if (!this.validation) return false;
+      this.confirmView = true;
+    },
+    closeCheckArea() {
+      this.confirmView = false;
+    }
+  },
+  computed: {
+    // 要件1の改修
+    validation: function() {
+      return this.formData.email === '' || this.formData.emailConfirm === '' || this.formData.email === this.formData.emailConfirm
+    },
+    // 要件3の改修
+    errorClass: function() {
+      return this.validation ? false : 'alert_bg';
+    }
+  },
 })
